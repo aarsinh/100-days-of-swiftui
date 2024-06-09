93da6c0c25510924ad7060df9aadf1fbf759dee6
aarsinh <aarsinh112@gmail.com>
2024-06-09 12:52:38 +0530

    added moonshot project

---
+++ 10-Moonshot/10-Moonshot/_0_MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0_MoonshotApp.swift
+//  10-Moonshot
+//
+//  Created by Aarav Sinha on 09/06/24.
+//
+
+import SwiftUI
+
+@main
+struct _0_MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
