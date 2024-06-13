ec7e01d8e4a07ad3ed1469579b25a8e6ec14f895
aarsinh <aarsinh112@gmail.com>
2024-06-13 16:08:40 +0530

    Project 9- Navigation

---
+++ 11-Navigation/11-Navigation/_1_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_NavigationApp.swift
+//  11-Navigation
+//
+//  Created by Aarav Sinha on 10/06/24.
+//
+
+import SwiftUI
+
+@main
+struct _1_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
