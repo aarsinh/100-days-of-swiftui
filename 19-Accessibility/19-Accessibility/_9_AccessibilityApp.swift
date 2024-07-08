cd0050038eb145baec61fb8230e6082287ebc8d4
aarsinh <148481261+aarsinh@users.noreply.github.com>
2024-07-08 14:20:57 +0530

    project 15

---
+++ 19-Accessibility/19-Accessibility/_9_AccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  _9_AccessibilityApp.swift
+//  19-Accessibility
+//
+//  Created by Aarav Sinha on 05/07/24.
+//
+
+import SwiftUI
+
+@main
+struct _9_AccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
